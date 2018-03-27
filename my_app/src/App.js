1a446aeaf518107a995cbeefbdf99cc4266d9aa8
Hannah Aurand <aurand.hannah@gmail.com>
2018-03-27 14:45:56 -0400

    Add links, routes, component files, start landing page component, install packages

---
+++ my_app/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
+import axios from 'axios'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <div className="navBar">
+            <nav>
+              <Link to="/">
+                <h1>Starving Artist</h1>
+              </Link>
+              <h5>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+              </h5>
+            </nav>
+          </div>
+          <Switch>
+            <LandingPage />
+            <Route path="/store" component={Store} />
+            <Route path="/store/show" component={Show} />
+            <Route path="/store/add" component={Add} />
+            <Route path="/store/edit" component={Edit} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
