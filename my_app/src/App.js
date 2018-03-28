0bcf85e3c97ed515b3a47913a37c08f5f4282c77
Brandon Sears <brandonsears8@gmail.com>
2018-03-28 11:41:05 -0400

    order routes

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -32,10 +32,10 @@ class App extends Component {
         <Switch>
           <Route path='/add-store' component={AddStore} />
           {/* <Route path='/:id/add' component={AddProduct} /> */}
-          <Route path='/:id/:product' component={ProductDetails} />
-          {/* <Route path='/:id/:product/edit' component={EditProduct} /> */}
-          <Route path='/:id' component={SingleStore} />
           {/* <Route path='/:id/edit' component={EditStore} /> */}
+          {/* <Route path='/:id/:product/edit' component={EditProduct} /> */}
+          <Route path='/:id/:product' component={ProductDetails} />
+          <Route path='/:id' component={SingleStore} />
           <Route path='/' component={StoreList} />
         </Switch>
       </div>
