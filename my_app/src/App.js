2ca04de2cfbcc48d0066d0567cc69a8d7b6b6c3e
Brandon Sears <brandonsears8@gmail.com>
2018-03-29 13:45:43 -0400

    remove test component

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -16,33 +16,30 @@ import ProductDetails from './ProductDetails/ProductDetails'
 import EditStore from './EditStore/EditStore'
 import EditProduct from './EditProduct/EditProduct'
 import AddProduct from './AddProduct/AddProduct'
-import Main from './MainTest/Main'
 
 class App extends Component {
-  render() {
-    // console.log(data[0].name)
+  render () {
     return (
-      <div className="container">
-        <div className="navBar">
+      <div className='container'>
+        <div className='navBar'>
           <nav>
-            <Link to="/" className="text-decoration">
-              <h1 className="pageTitle">Starving Artist</h1>
+            <Link to='/' className='text-decoration'>
+              <h1 className='pageTitle'>Starving Artist</h1>
             </Link>
-             <Link to="/add-Store" className="text-decoration-right">Create Store</Link>
-               <Link to="/login" className="text-decoration-right">Login</Link>
-               <Link to="/signup" className="text-decoration-right">Signup</Link>
+            <Link to='/add-Store' className='text-decoration-right'>Create Store</Link>
+            <Link to='/login' className='text-decoration-right'>Login</Link>
+            <Link to='/signup' className='text-decoration-right'>Signup</Link>
           </nav>
         </div>
         <Switch>
-          <Route path="/add-store" component={AddStore} />
-          <Route path="/:id/add" component={AddProduct} />
-          <Route path="/:id/edit" component={EditStore} />
-          <Route path="/:id/:product/edit" component={EditProduct} />
-          <Route path="/:id/:product" component={ProductDetails} />
-          <Route path="/:id" component={SingleStore} />
-          <Route path="/" component={StoreList} />
+          <Route path='/add-store' component={AddStore} />
+          <Route path='/:id/add' component={AddProduct} />
+          <Route path='/:id/edit' component={EditStore} />
+          <Route path='/:id/:product/edit' component={EditProduct} />
+          <Route path='/:id/:product' component={ProductDetails} />
+          <Route path='/:id' component={SingleStore} />
+          <Route path='/' component={StoreList} />
         </Switch>
-        <Main />
       </div>
     )
   }
