defae405ad0fe1f105f36231dbd9ca3e5be39dbb
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-03-29 09:19:46 -0400

    trying to pull files

--- my_app/src/AddStore/AddStore.js
+++ my_app/src/AddStore/AddStore.js
@@ -9,7 +9,6 @@ class AddStore extends Component {
   onSubmit(e) {
     e.preventDefault()
     console.log(e.target.name.value)
-<<<<<<< HEAD
     axios
       .post('https://starving-artist.herokuapp.com/api', {
         name: e.target.name.value,
@@ -17,28 +16,15 @@ class AddStore extends Component {
       })
       .then(res => console.log(res))
       .then(() => this.props.history.push('/'))
-=======
-    axios.post('https://starving-artist.herokuapp.com/api', {
-      name: e.target.name.value,
-      about: e.target.about.value
-    }).then(res => console.log(res))
-    .then(() => this.props.history.push('/'))
->>>>>>> b92974c289784a9227d2bdd1612b897fa6c1a91f
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.onSubmit}>
-<<<<<<< HEAD
           <input type="text" name="name" placeholder="name" />
           <input type="text" name="about" placeholder="about" />
           <input type="submit" value="ADD" />
-=======
-          <input type='text' name='name' placeholder='name' required />
-          <input type='text' name='about' placeholder='about' required />
-          <input type='submit' value='ADD' />
->>>>>>> b92974c289784a9227d2bdd1612b897fa6c1a91f
         </form>
       </div>
     )
