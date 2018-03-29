7e21f79121222fd7d00d34a9b96505531d3df46c
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-03-29 14:37:48 -0400

    working on forms

--- my_app/src/AddProduct/AddProduct.js
+++ my_app/src/AddProduct/AddProduct.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Forms from './Forms.css'
 
 class AddProduct extends Component {
   constructor () {
@@ -22,31 +23,26 @@ class AddProduct extends Component {
 
   render () {
     return (
-      <div>
+      <div className='container'>
         <form onSubmit={this.onSubmit}>
-          <p>
-            <label for='title'>Product Title</label>
-            <input type='text' name='title' />
-          </p>
-          <p>
-            <label for='artist'>Artitst</label>
-            <input type='text' name='artist' />
-          </p>
-          <p>
-            <label for='description'>Description</label>
-            <input type='text' name='description' />
-          </p>
-          <p>
-            <label for='Price'>price</label>
-            <input type='text' name='price' />
-          </p>
-          <p>
-            <label for='image'>Image</label>
-            <input type='text' name='image' />
-          </p>
-          <br />
+
+          <label for='title'>Title</label>
+          <input type='text' name='title' />
+
+          <label for='artist'>Artitst</label>
+          <input type='text' name='artist' />
+
+          <label for='description'>Description</label>
+          <input type='text' name='description' />
+
+          <label for='Price'>Price</label>
+          <input type='text' name='price' />
+
+          <label for='image'>Image</label>
+          <input type='text' name='image' />
           <input type='submit' value='ADD' />
         </form>
+       
       </div>
     )
   }
